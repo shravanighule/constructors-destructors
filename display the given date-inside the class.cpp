5b55fbedc46ffcba39cd4ae8a6804a31fd34900c
shravanighule <shravanighule16@gmail.com>
2024-09-05 11:39:46 +0530

    Add files via upload

---
+++ display the given date-inside the class.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Date{
+	int d;
+	int m;
+	int y;
+	
+	public:
+		Date()
+		{
+			cout<<"Todays Date is:"<<endl;
+			d=4;
+			m=9;
+			y=2024;
+			cout<<d<<"/"<<m<<"/"<<y;
+		}};
+		int main(){
+		Date today;
+		}
+		
+		/*
+		
+		Todays Date is:
+4/9/2024
+/*
